ed43ef2a6548066b7c55ef9f653932b5c7bb52d4
Dominic Pace <dpace@nhpace.net>
2017-02-16 00:04:54 -0500

    Added tests for Firefox, Safari, and Chrome Driver Direct Instances

---
+++ src/test/java/org/drivrhq/drivr/unit/browser/BrowserFactoryTests.java
@@ -0,0 +1,69 @@
+package org.drivrhq.drivr.unit.browser;
+
+import org.drivrhq.drivr.browser.BrowserFactory;
+import org.drivrhq.drivr.browser.enums.BrowserType;
+import org.drivrhq.drivr.browser.enums.DriverRunType;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+/**
+ * (C) Copyright 2016 Dominic Pace (https://github.com/Dominic-Pace)
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 2.1 which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/lgpl-2.1.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ */
+public class BrowserFactoryTests {
+
+    private BrowserFactory browserFactory;
+    private WebDriver driver;
+    private String driverType;
+
+    @AfterMethod
+    public void cleanUpAfterTest() {
+        driver.quit();
+    }
+
+    @Test
+    public void chromeDriverInstanceTest() {
+        browserFactory = new BrowserFactory(BrowserType.CHROME);
+
+        driver = browserFactory.getBrowserInstance(DriverRunType.DIRECT);
+        driverType = ((RemoteWebDriver) driver).getCapabilities().getBrowserName();
+
+        Assert.assertTrue(driverType.equals("chrome"),
+                "The browser type was " + driverType + " but was expected to be chrome.");
+    }
+
+    @Test
+    public void firefoxDriverInstanceTest() {
+        browserFactory = new BrowserFactory(BrowserType.FIREFOX);
+
+        driver = browserFactory.getBrowserInstance(DriverRunType.DIRECT);
+        driverType = ((RemoteWebDriver) driver).getCapabilities().getBrowserName();
+
+        Assert.assertTrue(driverType.equals("firefox"),
+                "The browser type was " + driverType + " but was expected to be firefox.");
+    }
+
+    @Test
+    public void safariDriverInstanceTest() {
+        browserFactory = new BrowserFactory(BrowserType.SAFARI);
+
+        driver = browserFactory.getBrowserInstance(DriverRunType.DIRECT);
+        driverType = ((RemoteWebDriver) driver).getCapabilities().getBrowserName();
+
+        Assert.assertTrue(driverType.equals("safari"),
+                "The browser type was " + driverType + " but was expected to be safari.");
+    }
+}
